7766a941d58d566e2d2f0da2683d8743ba3d3dcb
Franki Callard <francescalily@outlook.com>
2023-11-15 00:06:50 +0000

    final commit night

--- src/script.js
+++ src/script.js
@@ -18,8 +18,10 @@ const scene = new THREE.Scene();
 
 //need an object to add tweaks to gui
 const parameters = {};
-parameters.count = 1000;
-parameters.size = 0.02;
+parameters.count = 100000;
+parameters.size = 0.001;
+parameters.radius = 5;
+parameters.branches = 3;
 
 //have to use null because otherwise the parameters will not be destroyed
 let geometry = null;
@@ -38,9 +40,13 @@ const generateGalaxy = () => {
 
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3; //so every three positions in the array corresponds to one vertex
-    positions[i3 + 0] = (Math.random() - 0.5) * 3; // does x
-    positions[i3 + 1] = (Math.random() - 0.5) * 3; //does y coordinate
-    positions[i3 + 2] = (Math.random() - 0.5) * 3; //does z coordinate of the vertex
+
+    const radius = Math.random() * parameters.radius;
+    const branchAngle = i % parameters.branches;
+    const branches = Math.random() * parameters.branches;
+    positions[i3 + 0] = radius * branchAngle; // does x
+    positions[i3 + 1] = 0; //does y coordinate
+    positions[i3 + 2] = 0; //does z coordinate of the vertex
   }
   geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3)); //creates new attribute where 3 indicates that each vertex position is composed of 3 values
 
@@ -62,7 +68,7 @@ generateGalaxy();
 gui
   .add(parameters, "count")
   .min(100)
-  .max(10000)
+  .max(100000)
   .step(100)
   .onFinishChange(generateGalaxy);
 gui
@@ -71,6 +77,20 @@ gui
   .max(0.1)
   .step(0.001)
   .onFinishChange(generateGalaxy); //onFinishChange means it will change the galaxy when the user lets go of using the slider
+
+gui
+  .add(parameters, "radius")
+  .min(0.01)
+  .max(20)
+  .step(0.01)
+  .onFinishChange(generateGalaxy);
+
+gui
+  .add(parameters, "branches")
+  .min(2)
+  .max(20)
+  .step(1)
+  .onFinishChange(generateGalaxy);
 
 /**
  * Sizes
